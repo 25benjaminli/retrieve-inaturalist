3ab483b44ea6e0694a18ff8008d01dc35020d8b8
Benjamin <25benjaminli@gmail.com>
2022-12-10 20:53:31 -0500

    webscrape update

---
+++ README.md
@@ -0,0 +1,1 @@
+This is the program for webscraping inaturalist. Modify the list with URLs for the types of bacteria you would like to scrape.
--- run.py
+++ run.py
@@ -5,41 +5,51 @@ from selenium import webdriver
 import os
 from PIL import Image
 import time
-URL = "https://www.inaturalist.org/taxa/324242-Oscillatoria/browse_photos"
 
-browser = webdriver.ChromeOptions()
-browser.add_argument('headless')
-browser = webdriver.Chrome(options=browser)
+URLS = [
+    "https://www.inaturalist.org/taxa/324242-Oscillatoria/browse_photos",
 
-browser.get(URL)
-browser.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-time.sleep(0.5)
-browser.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-time.sleep(0.5)
-browser.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-time.sleep(0.5)
-browser.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+]
 
-soup = BeautifulSoup(browser.page_source, "html.parser")
 
-# soup.prettify()
-"""
-cover-image-https-inaturalist-open-data-s-3-amazonaws-com-photos-51218764-medium-jpg
-cover-image-https-inaturalist-open-data-s-3-amazonaws-com-photos-51218785-medium-jpg
-cover-image-https-inaturalist-open-data-s-3-amazonaws-com-photos-51218801-medium-jpg
-"""
-# print(soup.findAll('div'))
-# print(len(soup.find_all('div')))
+for URL in URLS:
+    species_name = URL.split("/")[-2]
+    print("scraping for " + species_name)
+    if not os.path.exists(f'images/{species_name}'):
+        os.makedirs(f'images/{species_name}')
+    
+    browser = webdriver.ChromeOptions()
+    browser.add_argument('headless')
+    browser = webdriver.Chrome(options=browser)
 
-results = soup.find_all('div', id=lambda x: x and x.startswith('cover-image-https-inaturalist-open-data-s-3-amazonaws-com-photos'))
+    # you need to scroll for the browser to load all the images
+    # TODO: find a way to automatically scroll to the bottom of the page with sensing as soon as you scroll again
 
-# print(results)
+    browser.get(URL)
+    prev_height = 0
+    while prev_height < browser.execute_script("return document.body.scrollHeight"):
+        prev_height = browser.execute_script("return document.body.scrollHeight")
+        browser.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+        time.sleep(0.5)
+        print("scrolling...")
+    soup = BeautifulSoup(browser.page_source, "html.parser")
 
-for result in results:
-    url = result["style"].split('url(')[1].split(')')[0].replace('\"', '')
-    print(url)
-    img = requests.get(url)
-    print(type(img))
-    file = open(f'images/{url.split("/")[-2]}-{url.split("/")[-1]}', 'wb')
-    file.write(img.content)
-    file.close()
+    
+    """
+    EXAMPLES:
+    cover-image-https-inaturalist-open-data-s-3-amazonaws-com-photos-51218764-medium-jpg
+    cover-image-https-inaturalist-open-data-s-3-amazonaws-com-photos-51218785-medium-jpg
+    cover-image-https-inaturalist-open-data-s-3-amazonaws-com-photos-51218801-medium-jpg
+    """
+
+    # cover results that partially match ID
+    results = soup.find_all('div', id=lambda x: x and x.startswith('cover-image-https-inaturalist-open-data-s-3-amazonaws-com-photos'))
+
+    for result in results:
+        url = result["style"].split('url(')[1].split(')')[0].replace('\"', '')
+        print(url)
+        img = requests.get(url)
+        print(type(img))
+        file = open(f'images/{species_name}/{url.split("/")[-2]}-{url.split("/")[-1]}', 'wb')
+        file.write(img.content)
+        file.close()
